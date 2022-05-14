95581dc128b1ddeb7cb7ed7e2bed1ef8007f7b40
Oshadha Vimukthi <105389394+OshadhaVimukthiB@users.noreply.github.com>
2022-05-14 11:18:02 +0530

    Add files via upload

---
+++ sk/index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Wondercrack</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End Plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="assets/images/favicon1.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:../../partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+        <a href="https://oshadha.net">
+          <h3>Wondercrack</h3>
+        </a>
+      </div>
+      <ul class="nav">
+        <li class="nav-item profile">
+          <div class="profile-desc">
+            <div class="profile-pic">
+              <div class="count-indicator">
+                <img class="img-xs rounded-circle " src="assets/images/puppy1.jpg" alt="">
+                <span class="count bg-success"></span>
+              </div>
+              <div class="profile-name">
+                <h5 class="mb-0 font-weight-normal">Wondercrack</h5>
+                
+              </div>
+            </div>
+        </li>
+        <li class="nav-item nav-category">
+          <span class="nav-link">HOME</span>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="dashboard.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-speedometer"></i>
+            </span>
+            <span class="menu-title">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item nav-category">
+          <span class="nav-link">STRIPE GATEWAYS</span>
+        </li>
+        
+
+        
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="scioriindex.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (INR ₹42)</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="scioriindex200.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (INR ₹200)</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="scuindex.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (USD $0.50)</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="scuindex3.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (USD $3)</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="sciindex.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (EUR €0.50)</span>
+          </a>
+        </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="sciindex3.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">CHARGE (EUR €3)</span>
+          </a>
+        </li>
+
+        
+
+        
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="saindex.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card"></i>
+            </span>
+            <span class="menu-title">STRIPE AUTH</span>
+          </a>
+        </li>
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="sk.php">
+            <span class="menu-icon">
+              <i class="mdi mdi-credit-card-multiple"></i>
+            </span>
+            <span class="menu-title">SK KEY CHECKER</span>
+          </a>
+        </li>
+      </ul>
+
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:../../partials/_navbar.html -->
+      <nav class="navbar p-0 fixed-top d-flex flex-row">
+        <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
+          <a href="dashboard.php">
+            <h1>Home</h1>
+          </a>
+        </div>
+        <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-menu"></span>
+          </button>
+          
+          <ul class="navbar-nav navbar-nav-right">
+            <li class="nav-item dropdown d-none d-lg-block">
+              <a class="nav-link btn btn-success create-new-button" id="createbuttonDropdown" data-toggle="dropdown" aria-expanded="false" href="https://oshadha.net">Oshadha Vimukthi</a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="createbuttonDropdown">
+            </li>
+
+
+            <li class="nav-item dropdown border-left">
+              
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+            <li class="nav-item dropdown">
+              <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
+                <div class="navbar-profile">
+                  <img class="img-xs rounded-circle" src="assets/images/puppy1.jpg" alt="">
+                  
+                  <i class="mdi mdi-menu-down d-none d-sm-block"></i>
+                </div>
+              </a>
+              
+            </li>
+          </ul>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="mdi mdi-format-line-spacing"></span>
+          </button>
+        </div>
+      </nav>
+
+      
+
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+       
+          <div class="row ">
+            <div class="col-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">OWNER : </h4>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>NAME</th>
+                          <th>CONTACT</th>
+                        </tr>
+                      </thead>
+                      <DBody>
+                       <tr>
+                          <td><strong>Oshadha Vimukthi</strong></td>
+                          <td><a href='https://oshadha.net'>Website</a></td>
+                        </tr>
+                      </DBody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="footer__block block no-margin-bottom">
+              <div class="container-fluid text-center">
+                <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+                <p class="no-margin-bottom">2021 &copy; All Rights Reserved <a href="https://oshadha.net">Oshadha Vimukthi</a>.</p>
+              </div>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+    <script src="assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="assets/js/off-canvas.js"></script>
+    <script src="assets/js/hoverable-collapse.js"></script>
+    <script src="assets/js/misc.js"></script>
+    <script src="assets/js/settings.js"></script>
+    <script src="assets/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="assets/js/dashboard.js"></script>
+    <!-- End custom js for this page -->
+</body>
+
+</html>
